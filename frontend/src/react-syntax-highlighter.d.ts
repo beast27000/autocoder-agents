2024ada6549833dfa3706a7d22f0d61043ca9708
Vishvvesh Nagappan <vishvvesh@gmail.com>
2026-01-20 12:18:48 +0530

    Fix TypeScript syntax error in declaration file

--- frontend/src/react-syntax-highlighter.d.ts
+++ frontend/src/react-syntax-highlighter.d.ts
@@ -16,5 +16,4 @@ declare module 'react-syntax-highlighter' {
 declare module 'react-syntax-highlighter/dist/esm/styles/hljs' {
   export const atomOneDark: any;
   export const atomOneLight: any;
-  [key: string]: any;
 }
